0afbbf139ec1969db908c4bcdb6c8e7f50c65b26
Anton Soroko <anton.soroko@gmail.com>
2017-05-05 16:53:01 +0300

    Added SwitchUserContext function.

--- user.go
+++ user.go
@@ -3,6 +3,7 @@ package gapi
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 )
 
@@ -12,6 +13,9 @@ type User struct {
 	Name    string
 	Login   string
 	IsAdmin bool
+}
+type SwitchUserContextResponse struct {
+	Message string
 }
 
 func (c *Client) Users() ([]User, error) {
@@ -36,4 +40,42 @@ func (c *Client) Users() ([]User, error) {
 		return users, err
 	}
 	return users, err
+}
+
+func (c *Client) SwitchUserContext(Id int64) (SwitchUserContextResponse, error) {
+	var message SwitchUserContextResponse
+	path := fmt.Sprintf("/api/user/using/%d", Id)
+	req, err := c.newRequest("POST", path, nil)
+	if err != nil {
+		return message, err
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		return message, err
+	}
+	switch resp.StatusCode {
+	case 200:
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return message, err
+		}
+		err = json.Unmarshal(data, &message)
+		if err != nil {
+			return message, err
+		}
+	case 401:
+		origErr := errors.New(resp.Status)
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return message, origErr
+		}
+		err = json.Unmarshal(data, &message)
+		if err != nil {
+			return message, origErr
+		}
+	default:
+		return message, errors.New(resp.Status)
+	}
+
+	return message, err
 }
